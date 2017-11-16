38fe77b101a9246f2a4a2bbd4fa3c96d482d47dc
Andras <andras@MacBook.local>
2017-11-17 10:07:21 +1100

    End of Lesson 104.

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Andras on 17/11/17.
+//  Copyright © 2017 Andras. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
